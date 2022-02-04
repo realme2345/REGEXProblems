4673c75fdbefa82a79bfee8e2ca6f0fd8435c1bc
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-04 14:53:32 +0530

    [Raja]Add validating a lastname implemented in REGEX

--- RegularExpressionsPracticeProblem/RegularExpressions.cs
+++ RegularExpressionsPracticeProblem/RegularExpressions.cs
@@ -11,16 +11,34 @@ namespace RegularExpressionsPracticeProblem
 /// </summary>
     class RegularExpressions
     {
+        //validatingFirst Name
         public static string FirstName = "^[A-Z][A-Z a-z]{3,}$"; // this pattern used for validate the name
         public void ValidateFirstName(string firstName) // this method is used for validating first name
         {
             if (Regex.IsMatch(firstName, FirstName))
             {
                 Console.WriteLine("{0} is valid", firstName);
+                Console.WriteLine("First Name :",firstName);
             }
             else
             {
                 Console.WriteLine("{0} is not valid", firstName);
+            }
+            //The End
+        }
+        //Validating The LastName
+        public static string LastName = "^[A-Z][A-Z a-z]{3,}$";// pattern used for checking as per use case
+        public void ValidateLastName(string lastName) // this method is used for validating last name
+        {
+            if (Regex.IsMatch(lastName, LastName))
+            {
+                Console.WriteLine("{0} is valid", lastName);
+                Console.WriteLine("Last Name :", lastName);
+
+            }
+            else
+            {
+                Console.WriteLine("{0} is not valid", lastName);
             }
             //The End
         }
